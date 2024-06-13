c281a75012770eb098733fa54cf86f9bcbc88b91
chenyixincyx <161786411+chenyixincyx@users.noreply.github.com>
2024-06-13 16:57:58 +0800

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,25 +1,30 @@
-package com.example.myapplication
+package com.example.r2
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.r2.ui.theme.R2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            R2Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("2207020323陈艺鑫")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
                 }
             }
         }
@@ -27,17 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $2207020323陈艺鑫!",
-            modifier = modifier
-    )
+fun BoxDemo(color:Color,context:String) {
+    Box(
+        modifier = Modifier
+            .width(133.dp)
+            .fillMaxHeight()
+            .background(color),
+        contentAlignment = Alignment.Center
+    ){this:BoxScope
+        Text(text = context)
+    }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+fun RowRGB() {
+    Row() { this:RowScope
+        BoxDemo(color = Red, context ="红色" )
+        BoxDemo(color = Green, context = "绿色")
+        BoxDemo(Color= Blue, context = "蓝色")
     }
 }
